dabca4ffa6a19e064456e393c37e28d038029487
JongPil Yoon <whdvlf94@gmail.com>
2020-05-29 18:32:36 +0900

    DB to JSON REST API Server(GET Method)

--- src/main/java/com/example/api/controller/DataController.java
+++ src/main/java/com/example/api/controller/DataController.java
@@ -1,46 +1,59 @@
 package com.example.api.controller;
 
 import com.example.api.service.DataRepository;
-import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/data")
+@RequestMapping("/{tableName}")
 public class DataController {
 
-    @Autowired
-    Environment env;
 
     @Autowired
     private DataRepository repository;
 
-    @GetMapping("/{tableName}")
-//    public Map<String, Object> getData(@PathVariable String tableName) throws SQLException, ClassNotFoundException, JSONException {
-//
-//        JSONArray jsonArray = repository.findAll(tableName);
-//        Map<String, Object> result = new HashMap<>();
-//        JSONObject jsonObject = (JSONObject)jsonArray.get(0);
-//        result.put("ID", jsonObject.get("ID"));
+    //사용자 전체 조회
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public String getData(@PathVariable String tableName) throws SQLException, ClassNotFoundException, JSONException {
-
         JSONArray jsonArray = repository.findAll(tableName);
-//        Map<Object, String> result = new HashMap<>();
-//        JSONObject jsonObject = (JSONObject)jsonArray.get(0);
-//        result.put("ID", jsonObject.get("ID"));
-
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(String.valueOf(jsonArray));
 
         return String.valueOf(jsonArray);
+
     }
+
+    @GetMapping(value = "/detail",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getDataByColNum(
+            @PathVariable String tableName,
+            HttpServletRequest request
+    ) throws SQLException, ClassNotFoundException, JSONException, UnsupportedEncodingException {
+        String queryString = URLDecoder.decode(request.getQueryString(), "UTF-8");
+
+        List<String> queryList = Arrays.asList(queryString.split("&"));
+        Map<String, Object> queryMap = new HashMap<>();
+        for (String query : queryList) {
+            String[] queryTuple = query.split("=");
+            queryMap.put(queryTuple[0], queryTuple[1]);
+        }
+
+        JSONArray jsonArrayCol = repository.findByColNum(tableName,queryMap);
+        return String.valueOf(jsonArrayCol);
+
+
+    }
+
 }
+
+
--- src/main/java/com/example/api/service/DataRepository.java
+++ src/main/java/com/example/api/service/DataRepository.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
+import java.util.*;
 
 
 @Repository
@@ -24,33 +25,76 @@ public class DataRepository {
     private Connection con;
     private ResultSet rs;
     private Statement stmt;
+    private ResultSetMetaData rsmd;
 
 
-    //테이블 조회
+    //테이블 전체 조회
     public JSONArray findAll(String tableName) throws SQLException, ClassNotFoundException, JSONException {
-        java.sql.Statement statement;
         JSONArray jsonArray = new JSONArray();
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         con = DriverManager.getConnection(url, username, password);
         stmt = con.createStatement();
 
-        String sql = "SELECT * FROM ";
+        String sql = "SELECT * FRM ";
         sql += tableName;
         rs = stmt.executeQuery(sql);
-        Integer i =0;
+        Integer i = 0;
         ResultSetMetaData rsmd = rs.getMetaData();
         int columnCount = rsmd.getColumnCount();
+
         while (rs.next()){
             JSONObject jsonObject = new JSONObject();
-            for (int ii = 1; ii < columnCount + 1; ii++ ) {
+        for (int ii = 1; ii < columnCount + 1; ii++) {
                 String alias = rsmd.getColumnLabel(ii);
+                jsonObject.put(alias, rs.getObject(alias));
+        }
+            jsonArray.put(i, jsonObject);
+            i++;
+        }
+
+        return jsonArray;
+    }
+
+    //테이블 특정 파라미터 값 조회
+    public JSONArray findByColNum(String tableName, Map<String, Object> queryMap) throws SQLException, ClassNotFoundException, JSONException {
+        JSONArray jsonArray = new JSONArray();
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection(url, username, password);
+        stmt = con.createStatement();
+
+
+
+        Integer i = 0;
+
+
+        String sql = "SELECT *";
+        sql += " FROM "+tableName;
+        sql += " where ";
+        int j = 0;
+        for(String mapkey : queryMap.keySet()){
+            sql += mapkey + " = " +  '"' + queryMap.get(mapkey).toString() + '"';
+            j ++;
+            if (queryMap.size() != j) {
+                sql += " AND ";
+                continue;
+            }
+        }
+        rs = stmt.executeQuery(sql);
+        ResultSetMetaData rsmdAll = rs.getMetaData();
+        int columnCountAll = rsmdAll.getColumnCount();
+
+        jsonArray = new JSONArray();
+        while (rs.next()){
+            JSONObject jsonObject = new JSONObject();
+            for (int ii = 1; ii < columnCountAll + 1; ii++) {
+                String alias = rsmdAll.getColumnLabel(ii);
                 jsonObject.put(alias, rs.getObject(alias));
             }
             jsonArray.put(i, jsonObject);
             i++;
         }
-
         return jsonArray;
     }
 }
